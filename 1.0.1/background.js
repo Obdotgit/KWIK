daa4282c89dd325e9f2a867b12618a64788864a9
Obdotgit <obidoesroblox@gmail.com>
2025-09-14 11:43:50 +0100

    Update background.js

--- 1.0.1/background.js
+++ 1.0.1/background.js
@@ -1,5 +1,7 @@
 chrome.commands.onCommand.addListener((command) => {
     if (command === "open_kwik") {
-        chrome.action.openPopup();
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, { action: "open_overlay" });
+        });
     }
 });
